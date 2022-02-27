94d907c90b166b9f9130fb2c3b2add3259813989
2021VER2021 <rogozhnikov-vas@yandex.ru>
2022-02-27 18:18:00 +0300

    not working version, attempt to do extra tasks

--- Dislocations_projects/dislocations_final/cmake-build-debug/cmake_install.cmake
+++ Dislocations_projects/dislocations_final/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Vasia&Misha/CLionProjects/untitled3
+# Install script for directory: C:/Users/Vasia&Misha/infa_2_sem_Rogozhnikov/Dislocations_projects/dislocations_final
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Vasia&Misha/CLionProjects/untitled3/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Vasia&Misha/infa_2_sem_Rogozhnikov/Dislocations_projects/dislocations_final/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- Dislocations_projects/dislocations_final/main.cpp
+++ Dislocations_projects/dislocations_final/main.cpp
@@ -1,4 +1,6 @@
 ///? maybe kill before step, maybe can spawn on edges, maybe...
+
+///9 - death; 0 - no; 1 - one; 2 - two...
 #include <iostream>
 #include <random>
 #include <chrono>
@@ -8,11 +10,14 @@ using namespace std;
 const int n_x_max = 100;             /// max width of crystal
 const int n_y_max = 100;             /// max height of crystal
 
-int n_x = 4;
-int n_y = 4;
+int n_x = 9; ///n+1
+int n_y = 9;
 
-const long test_number = 10000;     /// number of simulations
-const int disslocations = 1;    /// number of disslocations at the start
+const int empty = 0;
+const int death = 9;
+
+const long test_number = 1;     /// number of simulations
+const int disslocations = 10;    /// number of disslocations at the start
 
 int number_diss = 0; /// currant number of disslocations
 int steps;           /// currant number of steps
@@ -36,42 +41,46 @@ void start() {
     ///make start area, with no diss and stopped diss
     for (int i = 1; i < n_x + 1; i++) {
         for (int j = 1; j < n_y + 1; j++) {
-            field[i][j] = 1;
+            field[i][j] = empty;
         }
+    }
+    for (int i = 0; i < n_x + 2; i++) {
+        field[i][0] = death;
+        field[i][n_y + 1] = death;
+    }
+    for (int i = 0; i < n_y + 2; i++) {
+        field[0][i] = death;
+        field[n_x + 1][i] = death;
     }
 }
 
 void step(int rand_result, int x_coord, int y_coord) {
     switch (rand_result) {
         case 0: {
-            if (field[x_coord + 1][y_coord] == 1) {
-                field[x_coord][y_coord] = 1;
-                field[x_coord + 1][y_coord] = 2;
-                field_action[x_coord + 1][y_coord] = 1;
+            if (field[x_coord + 1][y_coord] != death) {
+                field[x_coord][y_coord] -= 1;
+                field[x_coord + 1][y_coord] += 1;
             }
             break;
         }
         case 1: {
-            if (field[x_coord - 1][y_coord] == 1) {
-                field[x_coord][y_coord] = 1;
-                field[x_coord - 1][y_coord] = 2;
-                field_action[x_coord - 1][y_coord] = 1;
+            if (field[x_coord - 1][y_coord] != death) {
+                field[x_coord][y_coord] -= 1;
+                field[x_coord - 1][y_coord] += 1;
             }
             break;
         }
         case 2: {
-            if (field[x_coord][y_coord + 1] == 1) {
-                field[x_coord][y_coord] = 1;
-                field[x_coord][y_coord + 1] = 2;
-                field_action[x_coord][y_coord + 1] = 1;
+            if (field[x_coord][y_coord + 1] != death) {
+                field[x_coord][y_coord] -= 1;
+                field[x_coord][y_coord + 1] += 1;
             }
             break;
         }
         case 3: {
-            if (field[x_coord][y_coord - 1] == 1) {
-                field[x_coord][y_coord] = 1;
-                field[x_coord][y_coord - 1] = 2;
-                field_action[x_coord][y_coord - 1] = 1;
+            if (field[x_coord][y_coord - 1] != death) {
+                field[x_coord][y_coord] -= 1;
+                field[x_coord][y_coord - 1] += 1;
             }
             break;
         }
@@ -80,11 +89,11 @@ void step(int rand_result, int x_coord, int y_coord) {
 
 bool check_for_kill(int x_coord, int y_coord) {
     bool f = true;
-    if (field[x_coord][y_coord] == 2) {
-        if (field[x_coord + 1][y_coord] != 1) { f = false; }
-        if (field[x_coord - 1][y_coord] != 1) { f = false; }
-        if (field[x_coord][y_coord + 1] != 1) { f = false; }
-        if (field[x_coord][y_coord - 1] != 1) { f = false; }
+    if (field[x_coord][y_coord] >= 1) {
+        if (field[x_coord + 1][y_coord] == death) { f = false; }
+        if (field[x_coord - 1][y_coord] == death) { f = false; }
+        if (field[x_coord][y_coord + 1] == death) { f = false; }
+        if (field[x_coord][y_coord - 1] == death) { f = false; }
     }
     return f;
 }
@@ -110,16 +119,15 @@ void mainloop() {
     start();
 
     /// put disslocations on a field
-    while (number_diss < disslocations) {
+    while (number_diss < disslocations) { ///at yhe start only one in a cell
         int x_coord = dist_n_x(rng);
         int y_coord = dist_n_y(rng);
-        if (field[x_coord][y_coord] == 1) {
-            field[x_coord][y_coord] = 2;
+        if (field[x_coord][y_coord] == empty) {
+            field[x_coord][y_coord] = 1;
             number_diss++;
         }
     }
 
-    // debug();
 
     /// main part
     while (number_diss > 0) {
@@ -127,26 +135,36 @@ void mainloop() {
         /// destroy action (every disslocation can potentially move)
         for (int i = 1; i < n_x + 1; i++) {
             for (int j = 1; j < n_y + 1; j++) {
-                field_action[i][j] = 0;
+                if (field[i][j] != death) {
+                    field_action[i][j] = field[i][j]; ///FIXIT the main issue!!! maybe rewrite all
+                }
+                else {
+                    field_action[i][j] = -1;
+                }
+
             }
         }
-
+        //debug();
         /// step stage, disslocations try to move
         for (int i = 1; i < n_x + 1; i++) {
-            for (int j = 1; j < n_y + 1; j++) {
-                if ((field[i][j] == 2) and (field_action[i][j] == 0)) {
-                    int result = dist(rng);
-                    step(result, i, j);
+            for (int j = 1; j < n_y + 1; j++) { ///while FIXIT
+                int m = field_action[i][j];
+                for (int k = 0; k < m; k++) {
+                    if  (field_action[i][j] > 0) {
+                        int result = dist(rng);
+                        step(result, i, j);
+                    }
+                    field_action[i][j] -= 1;
                 }
             }
         }
-        // debug();
+        debug();
 
         /// kill loop, merging disslocations to each other, or to edges
         for (int i = 1; i < n_x + 1; i++) {
             for (int j = 1; j < n_y + 1; j++) {
                 if (not(check_for_kill(i, j))) {
-                    field[i][j] = 0;
+                    field[i][j] = death;
                     number_diss -= 1;
                 }
             }
@@ -159,22 +177,18 @@ void mainloop() {
     /// increase sum
     sum = sum + steps;
 
-    // debug();
+    debug();
 }
 
 int main() {
-    for (int k = 3; k < 100; k++) {
-        sum = 0;
-        n_x = k;
-        n_y = k;
+    sum = 0;
 
-        /// do simulation 'test_number' times
-        for (long i = 0; i < test_number; i++) {
-            mainloop();
-        }
-
-        /// calculate average value, and show it
-        average = float(sum) / float(test_number);
-        cout << "[" << average << "," << k << "]," << endl;
+    /// do simulation 'test_number' times
+    for (long i = 0; i < test_number; i++) {
+        mainloop();
     }
+
+    /// calculate average value, and show it
+    average = float(sum) / float(test_number);
+    cout << average << endl;
 }
